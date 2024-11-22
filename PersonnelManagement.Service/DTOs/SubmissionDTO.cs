89099f7ef217369762549ba3a5b17988142b1b8e
Farhood Farid Etemad <farhood_fe@hotmail.com>
2024-11-22 13:15:53 +0330

    SubmissionDTO added

--- PersonnelManagement.Service/DTOs/SubmissionDTO.cs
+++ PersonnelManagement.Service/DTOs/SubmissionDTO.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace PersonnelManagement.Service.DTOs
 {
-    internal class SubmissionDTO
+    public class SubmissionDTO
     {
+        public long Id { get; set; }
+        public string? FieldValue { get; set; }
+        public long Fk_FieldDefinition { get; set; }
+        public long Fk_PersonInfo { get; set; }
     }
 }
