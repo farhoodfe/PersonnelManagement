0bb5db0ca26ee4eb9992631bda439f0b8666be39
Farhood Farid Etemad <farhood_fe@hotmail.com>
2024-11-22 20:14:24 +0330

    API: CreateField Action added

---
+++ PersonnelManagement.API/Controllers/DynamicFieldController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonnelManagement.API.Models;
+using PersonnelManagement.Service.Contracts;
+using PersonnelManagement.Service.DTOs;
+
+namespace PersonnelManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DynamicFieldController : ControllerBase
+    {
+        private readonly IFieldDefinitionService _FieldDefService;
+        private readonly IMapper _mapper;
+        public DynamicFieldController(IFieldDefinitionService FieldDefService, IMapper Mapper)
+        {
+            _FieldDefService = FieldDefService;       
+            _mapper = Mapper;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> CreateDynamicField ([FromBody] DynamicFieldModel FieldObj)
+        {
+            try
+            {
+                long resultId = 0;
+                if (FieldObj == null)
+                {
+                    return BadRequest("ابجکت ورودی نال است");
+                }
+                NewFieldDTO newField = _mapper.Map<NewFieldDTO>(FieldObj);
+                resultId = await _FieldDefService.CreateFieldAsync(newField);
+                if (resultId > 0)
+                    return Ok(resultId);
+                else
+                    return BadRequest("خطا");
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ PersonnelManagement.API/Models/DynamicFieldModel.cs
@@ -0,0 +1,13 @@
+﻿using static PersonnelManagement.Data.Statics.SD;
+
+namespace PersonnelManagement.API.Models
+{
+    public class DynamicFieldModel
+    {
+        public long? Id { get; set; }
+        public string? FieldName { get; set; }
+        public string? DisplayName { get; set; }
+        public FieldType? Type { get; set; }
+        public bool? IsRequired { get; set; }
+    }
+}
