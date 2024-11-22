c85169483cfaf16a2ac95039d39733244de7ad68
Farhood Farid Etemad <farhood_fe@hotmail.com>
2024-11-22 13:17:30 +0330

    PersonInfoDTO added

--- PersonnelManagement.Service/DTOs/PersonInfoDTO.cs
+++ PersonnelManagement.Service/DTOs/PersonInfoDTO.cs
@@ -8,5 +8,9 @@ namespace PersonnelManagement.Service.DTOs
 {
     public class PersonInfoDTO
     {
+        public long Id { get; set; }
+        public string? FName { get; set; } = null;
+        public string? LName { get; set; }
+        public string? PersonnelCode { get; set; }
     }
 }
