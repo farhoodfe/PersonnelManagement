246425b9abf6fa969230d9a1dee9e0289beaaff4
Farhood Farid Etemad <farhood_fe@hotmail.com>
2024-11-22 12:30:18 +0330

    DataAccess Entinties created

---
+++ PersonnelManagement.Data/Migrations/20241122085958_3entitiessadded.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonnelManagement.Data.Migrations
+{
+    public partial class _3entitiessadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "MIS");
+
+            migrationBuilder.CreateTable(
+                name: "FieldDefinition",
+                schema: "MIS",
+                columns: table => new
+                {
+                    pk_Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FieldName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FieldType = table.Column<int>(type: "int", nullable: true),
+                    IsRequired = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FieldDefinition", x => x.pk_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonInfo",
+                schema: "MIS",
+                columns: table => new
+                {
+                    pk_Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonnelCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonInfo", x => x.pk_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FieldSubmission",
+                schema: "MIS",
+                columns: table => new
+                {
+                    pk_Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FieldValue = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    fk_FieldDefinition = table.Column<long>(type: "bigint", nullable: false),
+                    fk_PersonInfo = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FieldSubmission", x => x.pk_Id);
+                    table.ForeignKey(
+                        name: "FK_FieldSubmission_FieldDefinition_fk_FieldDefinition",
+                        column: x => x.fk_FieldDefinition,
+                        principalSchema: "MIS",
+                        principalTable: "FieldDefinition",
+                        principalColumn: "pk_Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FieldSubmission_PersonInfo_fk_PersonInfo",
+                        column: x => x.fk_PersonInfo,
+                        principalSchema: "MIS",
+                        principalTable: "PersonInfo",
+                        principalColumn: "pk_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FieldSubmission_fk_FieldDefinition",
+                schema: "MIS",
+                table: "FieldSubmission",
+                column: "fk_FieldDefinition");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FieldSubmission_fk_PersonInfo",
+                schema: "MIS",
+                table: "FieldSubmission",
+                column: "fk_PersonInfo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FieldSubmission",
+                schema: "MIS");
+
+            migrationBuilder.DropTable(
+                name: "FieldDefinition",
+                schema: "MIS");
+
+            migrationBuilder.DropTable(
+                name: "PersonInfo",
+                schema: "MIS");
+        }
+    }
+}
---
+++ PersonnelManagement.Data/PersonnelDBContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonnelManagement.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelManagement.Data
+{
+    public class PersonnelDBContext : DbContext
+    {
+        //public PersonnelDBContext(DbContextOptions<PersonnelDBContext> options) : base(options)
+        //{
+            
+        //}
+
+        public DbSet<FieldDefinition> Fields { get; set; }
+        public DbSet<FieldSubmission> Submissions { get; set; }
+        public DbSet<PersonInfo> PersonInfos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //setting DB Connection string
+            optionsBuilder.UseSqlServer(@"server=localhost; Database=Personnel;Trusted_Connection = True;");
+            //optionsBuilder.UseLazyLoadingProxies();
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PersonnelDBContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
