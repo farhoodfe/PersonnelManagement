0ea53e5456450a7ab2c807f37928af63b4ea3360
Farhood Farid Etemad <farhood_fe@hotmail.com>
2024-11-29 20:41:53 +0330

    FormulaObjectAdded :Data

---
+++ PersonnelManagement.Data/Entities/Formula.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelManagement.Data.Entities
+{
+    /// <summary>
+    /// جدول ذخیره مقادیر فرمول بصورت رشته
+    /// </summary>
+    [Table("Formula", Schema = "MIS")]
+    public class Formula
+    {
+        [Key]
+        [Column("pk_Id")]
+        public long Id { get; set; }
+
+        [Column("FormulaName")]
+        [DefaultValue("")]
+        public string? FormulaName { get; set; }
+
+        [Column("FormulaText")]
+        [DefaultValue(" ")]
+        public string FormulaText { get; set; } = "";
+
+    }
+
+}
